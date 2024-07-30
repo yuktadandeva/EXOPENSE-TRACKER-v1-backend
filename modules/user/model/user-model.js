ab8fb54c549d2c0a786c62154475a18539cda032
Yukta <yuktadandeva@gmail.com>
2024-07-30 23:09:45 +0530

    schema update

--- modules/user/model/user-model.js
+++ modules/user/model/user-model.js
@@ -1,5 +1,13 @@
 import mongoose, { SchemaTypes } from "mongoose";
 
+const friendListSchema = mongoose.Schema({
+    name:{type: SchemaTypes.String},
+    userId:{type: SchemaTypes.Mixed},
+    userImg:{type: SchemaTypes.Mixed}, 
+    email: {type: SchemaTypes.Mixed},
+    phoneNumber: {type: SchemaTypes.String, max:10},   
+})
+
 const userSchema = mongoose.Schema({
     name: {type: SchemaTypes.String, required: true},
     userId: {type: SchemaTypes.Mixed, required: true, unique: true },
@@ -7,7 +15,7 @@ const userSchema = mongoose.Schema({
     userImg:{type: SchemaTypes.Mixed},
     phoneNumber: {type: SchemaTypes.String, max:10, required: true},
     password: {type: SchemaTypes.Mixed, required: true},
-    friendList: {type: [SchemaTypes.Mixed], default: []}
+    friendList: {type:[friendListSchema], default:[]}
 })
 
 export const userModel = mongoose.model('users', userSchema);
